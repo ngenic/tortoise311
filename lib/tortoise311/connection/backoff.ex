bcd3ddb2e4547154949bde9dea6d89a0d755aca7
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2021-11-19 10:47:37 -0500

    Testing

--- lib/tortoise311/connection/backoff.ex
+++ lib/tortoise311/connection/backoff.ex
@@ -1,6 +1,8 @@
 defmodule Tortoise311.Connection.Backoff do
   @moduledoc false
 
+  require Logger
+
   defstruct min_interval: 100, max_interval: 30_000, value: nil
   alias __MODULE__, as: State
 
@@ -8,9 +10,14 @@ defmodule Tortoise311.Connection.Backoff do
   Create an opaque data structure that describe a incremental
   back-off.
   """
+
   def new(opts) do
     min_interval = Keyword.get(opts, :min_interval, 100)
     max_interval = Keyword.get(opts, :max_interval, 30_000)
+
+    Logger.info(
+      "[Tortoise311] Backoff min_interval = #{min_interval} nax_interval = #{max_interval}"
+    )
 
     %State{min_interval: min_interval, max_interval: max_interval}
   end
